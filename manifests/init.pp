b940d5efc3bfc54eb8e9e1530a2aac202b066291
Martin Loy <martinloy.uy@gmail.com>
2014-11-24 16:01:54 -0200

    run geoipupdate after install

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,13 +8,16 @@ class geoip inherits geoip::params {
 
   class {'geoip::conf':
     require => Package[$geoip::params::package]
+  } -> exec {'run geoipupdate':
+    command => '/usr/bin/geoipupdate',
+    onlyif  => '/usr/bin/test -f ! /usr/share/GeoIP/GeoIPCity.dat'
   }
 
   if ($geoip::params::update_cron) {
 
-    file{'/etc/cron.weekly/geoip-update':
+    file {'/etc/cron.weekly/geoip-update':
         ensure => present,
-        source => 'puppet:///geoip/geoipupdate.cron'
+        source => 'puppet:///modules/geoip/geoipupdate.cron'
     }
 
   }
